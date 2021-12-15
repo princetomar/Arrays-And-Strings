59c3bb6000193f533d98d34ce8438f7b341fceaa
Prince Tomar <71916212+princetomar@users.noreply.github.com>
2021-12-15 13:30:15 +0530

    Add files via upload
    LeetCode link - https://leetcode.com/problems/happy-number/

---
+++ Overhappy_numbers.java
@@ -0,0 +1,53 @@
+package Arrays_Strings;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Overhappy_numbers
+{
+
+    // Method to update n
+    static int update(int n)
+    {
+        int out =0;
+
+        while (n > 0)
+        {
+            int digit = n %10;
+            out = out+ digit*digit;
+            n = n/10;
+
+        }
+        return out;
+    }
+    // this will return true if a no. is an overhappy no. or else return false
+    public static boolean check(Set<Integer> set, int n)
+    {
+        if (n == 1)
+            return true;
+
+        if (set.contains(n))
+        {
+            return  false;
+        }
+
+        set.add(n);
+        n = update(n);
+        return check(set, n);
+    }
+
+    static boolean overhappy(int n)
+    {
+        Set<Integer> set = new HashSet<>();
+        return check(set,n );
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("enter a number to check it's overhappy or not : ");
+        int n = scan.nextInt();
+        System.out.print(n+ " is ");
+        System.out.println(overhappy(n));
+    }
+}
