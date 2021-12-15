a3b54a2d0eb8b5c21c315fe964bdf94c35a01a07
Prince Tomar <71916212+princetomar@users.noreply.github.com>
2021-12-15 13:35:03 +0530

    Add files via upload

---
+++ Overlapping_Rectangles.java
@@ -0,0 +1,15 @@
+package Arrays_Strings;
+
+public class Overlapping_Rectangles
+{
+    static boolean overlappingRect(int arr1[], int arr2[])
+    {
+        return  !((arr1[2] <= arr2[0]) || (arr1[1]>=arr2[3]) || (arr1[0] >= arr2[2]) || (arr1[3] <= arr2[1]));
+    }
+
+    public static void main(String[] args) {
+        int arr1[] = {0,0,1,1};
+        int arr2[] = {1,0,2,1};
+        System.out.println(overlappingRect(arr1,arr2));
+    }
+}
